d5f09042a32022c7bb2c7c529a3f2bd64359be21
Qmemorais <40478012+Qmemorais@users.noreply.github.com>
2021-05-13 20:17:43 +0300

    Add files via upload

---
+++ Test_Task/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Get_Info get_Info = new Get_Info();
+            Console.Write("Enter URL: ");
+            string url_ = Console.ReadLine();
+            get_Info.FoundAllHTML(url_);
+        }
+    }
+}
